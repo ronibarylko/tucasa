e19b3e75b7dec8143ddaff4c50209a59757d9609
Pablo Alcain <pabloalcain@gmail.com>
2020-03-21 09:42:44 -0300

    Cambia 2 espacios por 4 en indent

--- tests/scrapers/test_zonaprop.py
+++ tests/scrapers/test_zonaprop.py
@@ -3,7 +3,6 @@ import os
 
 from tucasa.parsers import zonaprop
 
-
 url_departamento = ("https://www.zonaprop.com.ar/propiedades/sensacional-"
                     "vista-a-parque-rivadavia.-amoblado.-45628502.html")
 url_listado = ("https://www.zonaprop.com.ar/departamentos-alquiler-"
@@ -20,155 +19,150 @@ descarga_listado = os.path.join(directorio, "../resources/listado.html")
 
 
 class TestZonaProp(unittest.TestCase):
-  def setUp(self):
-    pass
+    def setUp(self):
+        pass
 
-  def tearDown(self):
-    pass
+    def tearDown(self):
+        pass
 
-  def atest_parser_propiedad_true(self):
-    departamento = zonaprop.Propiedad(url_departamento)
-    prop = departamento._es_propiedad
-    self.assertTrue(prop)
+    def atest_parser_propiedad_true(self):
+        departamento = zonaprop.Propiedad(url_departamento)
+        prop = departamento._es_propiedad
+        self.assertTrue(prop)
 
-  def atest_parser_propiedad_false(self):
-    with self.assertWarns(UserWarning):
-      propiedad = zonaprop.Propiedad(url_listado)
-    prop = propiedad._es_propiedad
-    self.assertFalse(prop)
+    def atest_parser_propiedad_false(self):
+        with self.assertWarns(UserWarning):
+            propiedad = zonaprop.Propiedad(url_listado)
+        prop = propiedad._es_propiedad
+        self.assertFalse(prop)
 
-  def test_parser_propiedad_local_true(self):
-    departamento = zonaprop.Propiedad(descarga_departamento, True)
-    prop = departamento._es_propiedad
-    self.assertTrue(prop)
+    def test_parser_propiedad_local_true(self):
+        departamento = zonaprop.Propiedad(descarga_departamento, True)
+        prop = departamento._es_propiedad
+        self.assertTrue(prop)
 
-  def test_parser_propiedad_local_false(self):
-    with self.assertWarns(UserWarning):
-      propiedad = zonaprop.Propiedad(descarga_listado, True)
-    prop = propiedad._es_propiedad
-    self.assertFalse(prop)
+    def test_parser_propiedad_local_false(self):
+        with self.assertWarns(UserWarning):
+            propiedad = zonaprop.Propiedad(descarga_listado, True)
+        prop = propiedad._es_propiedad
+        self.assertFalse(prop)
 
-  def test_ambientes_1(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento, True)
-    ambientes = propiedad.ambientes
-    self.assertEqual(ambientes, 4)
+    def test_ambientes_1(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento, True)
+        ambientes = propiedad.ambientes
+        self.assertEqual(ambientes, 4)
 
-  def test_ambientes_2(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento2, True)
-    ambientes = propiedad.ambientes
-    self.assertEqual(ambientes, 2)
+    def test_ambientes_2(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento2, True)
+        ambientes = propiedad.ambientes
+        self.assertEqual(ambientes, 2)
 
-  def test_sup_cubierta_1(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento, True)
-    sup_cubierta = propiedad.superficie_cubierta
-    self.assertEqual(sup_cubierta, 138)
+    def test_sup_cubierta_1(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento, True)
+        sup_cubierta = propiedad.superficie_cubierta
+        self.assertEqual(sup_cubierta, 138)
 
-  def test_sup_cubierta_2(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento2, True)
-    sup_cubierta = propiedad.superficie_cubierta
-    self.assertEqual(sup_cubierta, 51)
+    def test_sup_cubierta_2(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento2, True)
+        sup_cubierta = propiedad.superficie_cubierta
+        self.assertEqual(sup_cubierta, 51)
 
-  def test_sup_total_1(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento, True)
-    sup_total = propiedad.superficie_total
-    self.assertEqual(sup_total, 150)
+    def test_sup_total_1(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento, True)
+        sup_total = propiedad.superficie_total
+        self.assertEqual(sup_total, 150)
 
-  def test_sup_total_2(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento2, True)
-    sup_total = propiedad.superficie_total
-    self.assertEqual(sup_total, 54)
+    def test_sup_total_2(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento2, True)
+        sup_total = propiedad.superficie_total
+        self.assertEqual(sup_total, 54)
 
-  def test_banios_1(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento, True)
-    banios = propiedad.banios
-    self.assertEqual(banios, 2)
+    def test_banios_1(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento, True)
+        banios = propiedad.banios
+        self.assertEqual(banios, 2)
 
-  def test_banios_2(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento2, True)
-    banios = propiedad.banios
-    self.assertEqual(banios, 1)
+    def test_banios_2(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento2, True)
+        banios = propiedad.banios
+        self.assertEqual(banios, 1)
 
-  def test_dormitorios_1(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento, True)
-    dormitorios = propiedad.dormitorios
-    self.assertEqual(dormitorios, 3)
+    def test_dormitorios_1(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento, True)
+        dormitorios = propiedad.dormitorios
+        self.assertEqual(dormitorios, 3)
 
-  def test_dormitorios_2(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento2, True)
-    dormitorios = propiedad.dormitorios
-    self.assertEqual(dormitorios, 1)
+    def test_dormitorios_2(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento2, True)
+        dormitorios = propiedad.dormitorios
+        self.assertEqual(dormitorios, 1)
 
-  def test_antiguedad_1(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento, True)
-    antiguedad = propiedad.antiguedad
-    self.assertEqual(antiguedad, 80)
+    def test_antiguedad_1(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento, True)
+        antiguedad = propiedad.antiguedad
+        self.assertEqual(antiguedad, 80)
 
-  def test_antiguedad_2(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento2, True)
-    antiguedad = propiedad.antiguedad
-    self.assertEqual(antiguedad, 22)
+    def test_antiguedad_2(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento2, True)
+        antiguedad = propiedad.antiguedad
+        self.assertEqual(antiguedad, 22)
 
-  def test_antiguedad_2(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento3, True)
-    antiguedad = propiedad.antiguedad
-    self.assertEqual(antiguedad, 0)
+    def test_disposicion_1(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento, True)
+        disposicion = propiedad.disposicion
+        self.assertEqual(disposicion, "Frente")
 
-  def test_disposicion_1(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento, True)
-    disposicion = propiedad.disposicion
-    self.assertEqual(disposicion, "Frente")
+    def test_disposicion_2(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento2, True)
+        disposicion = propiedad.disposicion
+        self.assertEqual(disposicion, "Contrafrente")
 
-  def test_disposicion_2(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento2, True)
-    disposicion = propiedad.disposicion
-    self.assertEqual(disposicion, "Contrafrente")
+    def test_orientacion_1(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento, True)
+        orientacion = propiedad.orientacion
+        self.assertEqual(orientacion, "O")
 
-  def test_orientacion_1(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento, True)
-    orientacion = propiedad.orientacion
-    self.assertEqual(orientacion, "O")
+    def test_orientacion_2(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento2, True)
+        orientacion = propiedad.orientacion
+        self.assertEqual(orientacion, "NO")
 
-  def test_orientacion_2(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento2, True)
-    orientacion = propiedad.orientacion
-    self.assertEqual(orientacion, "NO")
+    def test_estado_1(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento, True)
+        estado = propiedad.estado
+        self.assertEqual(estado, "Excelente")
 
-  def test_estado_1(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento, True)
-    estado = propiedad.estado
-    self.assertEqual(estado, "Excelente")
+    def test_estado_2(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento2, True)
+        estado = propiedad.estado
+        self.assertEqual(estado, "Excelente")
 
-  def test_estado_2(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento2, True)
-    estado = propiedad.estado
-    self.assertEqual(estado, "Excelente")
+    def test_luminosidad_1(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento, True)
+        luminosidad = propiedad.luminosidad
+        self.assertEqual(luminosidad, "Muy luminoso")
 
-  def test_luminosidad_1(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento, True)
-    luminosidad = propiedad.luminosidad
-    self.assertEqual(luminosidad, "Muy luminoso")
+    def test_luminosidad_2(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento2, True)
+        luminosidad = propiedad.luminosidad
+        self.assertEqual(luminosidad, "Muy luminoso")
 
-  def test_luminosidad_2(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento2, True)
-    luminosidad = propiedad.luminosidad
-    self.assertEqual(luminosidad, "Muy luminoso")
+    def test_alquiler_1(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento, True)
+        alquiler = propiedad.alquiler
+        self.assertEqual(alquiler, "USD 1.300")
 
-  def test_alquiler_1(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento, True)
-    alquiler = propiedad.alquiler
-    self.assertEqual(alquiler, "USD 1.300")
+    def test_alquiler_2(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento2, True)
+        alquiler = propiedad.alquiler
+        self.assertEqual(alquiler, "$ 26.000")
 
-  def test_alquiler_2(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento2, True)
-    alquiler = propiedad.alquiler
-    self.assertEqual(alquiler, "$ 26.000")
+    def test_expensas_1(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento, True)
+        expensas = propiedad.expensas
+        self.assertEqual(expensas, "$ 6.400")
 
-  def test_expensas_1(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento, True)
-    expensas = propiedad.expensas
-    self.assertEqual(expensas, "$ 6.400")
-
-  def test_expensas_2(self):
-    propiedad = zonaprop.Propiedad(descarga_departamento2, True)
-    expensas = propiedad.expensas
-    self.assertEqual(expensas, "$ 8.000")
+    def test_expensas_2(self):
+        propiedad = zonaprop.Propiedad(descarga_departamento2, True)
+        expensas = propiedad.expensas
+        self.assertEqual(expensas, "$ 8.000")
--- tucasa/parsers/zonaprop.py
+++ tucasa/parsers/zonaprop.py
@@ -4,124 +4,125 @@ import warnings
 
 
 class Propiedad(object):
-  """
-  Parser de propiedades de ZonaProp
-  """
-  def __init__(self, url: str, local=False):
-    if not local:
-      response = requests.get(url).text
-    else:
-      response = open(url).read()
-    self.soup = bs4.BeautifulSoup(response, 'html.parser')
-    if not self._es_propiedad:
-      warnings.warn(f"{url} no parece ser una propiedad.", UserWarning)
+    """
+    Parser de propiedades de ZonaProp
+    """
 
-    def quitar_m2(entrada: str) -> int:
-      indice = entrada.find("m²")
-      valor = entrada[:indice]
-      valor = int(valor)
-      return valor
+    def __init__(self, url: str, local=False):
+        if not local:
+            response = requests.get(url).text
+        else:
+            response = open(url).read()
+        self.soup = bs4.BeautifulSoup(response, 'html.parser')
+        if not self._es_propiedad:
+            warnings.warn(f"{url} no parece ser una propiedad.", UserWarning)
 
-    def antiguedad(entrada: str) -> int:
-      if entrada == "A estrenar":
-        anios = 0
-      else:
-        anios = int(entrada)
-      return anios
+        def quitar_m2(entrada: str) -> int:
+            indice = entrada.find("m²")
+            valor = entrada[:indice]
+            valor = int(valor)
+            return valor
 
-    self._procesar_valor_conocidas = {}
-    self._procesar_valor_conocidas['Ambientes'] = lambda x: int(x)
-    self._procesar_valor_conocidas['Baños'] = lambda x: int(x)
-    self._procesar_valor_conocidas['Dormitorios'] = lambda x: int(x)
-    self._procesar_valor_conocidas['Superficie total'] = quitar_m2
-    self._procesar_valor_conocidas['Superficie cubierta'] = quitar_m2
-    self._procesar_valor_conocidas['Antigüedad'] = antiguedad
+        def antiguedad(entrada: str) -> int:
+            if entrada == "A estrenar":
+                anios = 0
+            else:
+                anios = int(entrada)
+            return anios
 
-    self._procesar_clave_conocidas = {}
-    self._procesar_clave_conocidas['Baño'] = lambda x: x + "s"
-    self._procesar_clave_conocidas['Ambiente'] = lambda x: x + "s"
-    self._procesar_clave_conocidas['Dormitorio'] = lambda x: x + "s"
+        self._procesar_valor_conocidas = {}
+        self._procesar_valor_conocidas['Ambientes'] = lambda x: int(x)
+        self._procesar_valor_conocidas['Baños'] = lambda x: int(x)
+        self._procesar_valor_conocidas['Dormitorios'] = lambda x: int(x)
+        self._procesar_valor_conocidas['Superficie total'] = quitar_m2
+        self._procesar_valor_conocidas['Superficie cubierta'] = quitar_m2
+        self._procesar_valor_conocidas['Antigüedad'] = antiguedad
 
-    self._informacion = {}
+        self._procesar_clave_conocidas = {}
+        self._procesar_clave_conocidas['Baño'] = lambda x: x + "s"
+        self._procesar_clave_conocidas['Ambiente'] = lambda x: x + "s"
+        self._procesar_clave_conocidas['Dormitorio'] = lambda x: x + "s"
 
-  @property
-  def _es_propiedad(self) -> bool:
-    es_propiedad = self.soup.body['id'].upper() == 'PROPERTY'
-    return es_propiedad
+        self._informacion = {}
 
-  @property
-  def informacion(self) -> dict:
-    datos = self.soup.findAll('li', {'class': 'icon-feature'})
-    _informacion = {}
-    for dato in datos:
-      clave = dato.span.text
-      clave = self._procesar_clave(clave)(clave)
-      valor = self._procesar_valor(clave)(dato.b.text)
-      _informacion[clave] = valor
-    alquiler = self.soup.find('div', {'class': 'price-items'})
-    _informacion["Alquiler"] = alquiler.span.text
-    expensas = self.soup.find('div', {'class': 'block-expensas block-row'})
-    _informacion["Expensas"] = expensas.span.text
-    return _informacion
+    @property
+    def _es_propiedad(self) -> bool:
+        es_propiedad = self.soup.body['id'].upper() == 'PROPERTY'
+        return es_propiedad
 
-  @property
-  def ambientes(self) -> int:
-    return self.informacion["Ambientes"]
+    @property
+    def informacion(self) -> dict:
+        datos = self.soup.findAll('li', {'class': 'icon-feature'})
+        _informacion = {}
+        for dato in datos:
+            clave = dato.span.text
+            clave = self._procesar_clave(clave)(clave)
+            valor = self._procesar_valor(clave)(dato.b.text)
+            _informacion[clave] = valor
+        alquiler = self.soup.find('div', {'class': 'price-items'})
+        _informacion["Alquiler"] = alquiler.span.text
+        expensas = self.soup.find('div', {'class': 'block-expensas block-row'})
+        _informacion["Expensas"] = expensas.span.text
+        return _informacion
 
-  @property
-  def antiguedad(self) -> int:
-    return self.informacion["Antigüedad"]
+    @property
+    def ambientes(self) -> int:
+        return self.informacion["Ambientes"]
 
-  @property
-  def superficie_total(self) -> int:
-    return self.informacion["Superficie total"]
+    @property
+    def antiguedad(self) -> int:
+        return self.informacion["Antigüedad"]
 
-  @property
-  def superficie_cubierta(self) -> int:
-    return self.informacion["Superficie cubierta"]
+    @property
+    def superficie_total(self) -> int:
+        return self.informacion["Superficie total"]
 
-  @property
-  def banios(self) -> int:
-    return self.informacion["Baños"]
+    @property
+    def superficie_cubierta(self) -> int:
+        return self.informacion["Superficie cubierta"]
 
-  @property
-  def dormitorios(self) -> int:
-    return self.informacion["Dormitorios"]
+    @property
+    def banios(self) -> int:
+        return self.informacion["Baños"]
 
-  @property
-  def disposicion(self) -> int:
-    return self.informacion["Disposición"]
+    @property
+    def dormitorios(self) -> int:
+        return self.informacion["Dormitorios"]
 
-  @property
-  def orientacion(self) -> int:
-    return self.informacion["Orientación"]
+    @property
+    def disposicion(self) -> int:
+        return self.informacion["Disposición"]
 
-  @property
-  def estado(self) -> int:
-    return self.informacion["Estado del inmueble"]
+    @property
+    def orientacion(self) -> int:
+        return self.informacion["Orientación"]
 
-  @property
-  def luminosidad(self) -> int:
-    return self.informacion["Luminosidad"]
+    @property
+    def estado(self) -> int:
+        return self.informacion["Estado del inmueble"]
 
-  @property
-  def alquiler(self) -> int:
-    return self.informacion["Alquiler"]
+    @property
+    def luminosidad(self) -> int:
+        return self.informacion["Luminosidad"]
 
-  @property
-  def expensas(self) -> int:
-    return self.informacion["Expensas"]
+    @property
+    def alquiler(self) -> int:
+        return self.informacion["Alquiler"]
 
-  def _procesar_valor(self, clave):
-    try:
-      funcion = self._procesar_valor_conocidas[clave]
-    except KeyError:
-      funcion = lambda x: x
-    return funcion
-      
-  def _procesar_clave(self, clave):
-    try:
-      funcion = self._procesar_clave_conocidas[clave]
-    except KeyError:
-      funcion = lambda x: x
-    return funcion
+    @property
+    def expensas(self) -> int:
+        return self.informacion["Expensas"]
+
+    def _procesar_valor(self, clave):
+        try:
+            funcion = self._procesar_valor_conocidas[clave]
+        except KeyError:
+            funcion = lambda x: x
+        return funcion
+
+    def _procesar_clave(self, clave):
+        try:
+            funcion = self._procesar_clave_conocidas[clave]
+        except KeyError:
+            funcion = lambda x: x
+        return funcion
